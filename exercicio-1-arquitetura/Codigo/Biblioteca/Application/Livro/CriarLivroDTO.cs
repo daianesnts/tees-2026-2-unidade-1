e290524de60303449c61822e2b0648e0662c784a
Hiakewve Santos <hiakewvealves@gmail.com>
2026-09-11 20:51:30 -0300

    cria classe CriarLivroDTO

---
+++ exercicio-1-arquitetura/Codigo/Biblioteca/Application/Livro/CriarLivroDTO.cs
@@ -0,0 +1,11 @@
+namespace Application.Livro.DTOs;
+
+public class CriarLivroDTO
+{
+    public string Isbn {get; set;} = null!;
+    public uint IdEditora {get; set;}
+    public string Nome {get; set;} = null!;
+    public DateTime? DataPublicacao {get; set;}
+    public string? Resumo {get; set;}
+    public byte[]? FotoCapa {get; set;}
+}
