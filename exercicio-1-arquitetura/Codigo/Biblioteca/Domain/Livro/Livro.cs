60367819021ccc7a35da39a0bcd3dd2ca8dd235a
Pauloisc <pauloitalo061@gmail.com>
2026-09-11 13:49:58 -0300

    Modificando nome de "LivroEntity.cs" para "Livro.cs"

---
+++ exercicio-1-arquitetura/Codigo/Biblioteca/Domain/Livro/Livro.cs
@@ -0,0 +1,12 @@
+public class LivroEntity
+{
+    public uint Id { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string? Subtitulo { get; set; }
+
+    public string? Isbn { get; set; }
+
+    public uint? EditoraId { get; set; }
+}
